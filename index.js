c4bde532cb52b6560ef721c5b2af93995d217120
rogeriomussoco <rogeriotiagomiguelit@gmail.com>
2022-01-21 09:22:16 -0300

    ultimo commit

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log('I am writing Typescript: Eu estou escrevendo Typescripe');
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var primStr = (0, readline_sync_1.question)('Insira o primeiro numero:\n');
+    var operator = (0, readline_sync_1.question)('Insira o operador:\n');
+    var segString = (0, readline_sync_1.question)('Insira o segundo numero:\n');
+    //console.log('primStr');
+    var validInput = isNumber(primStr) && isOperador(operator) && isNumber(segString);
+    console.log(validInput);
+    if (validInput) { //console.log('isvalid.')
+        var firstNum = parseInt(primStr);
+        var secondNum = parseInt(segString);
+        var resultado = calculate(firstNum, operator, secondNum);
+        console.log(resultado);
+    }
+    else {
+        console.log('\ninvalid input\n');
+        main();
+    }
+    //console.log (primString, operador, segString);
+    //const firstNum = isNumber(primStr);
+    //console.log(firstNum);
+    //const op = eOperador(operador)
+    //console.log(op);
+    //const validInput:boolean = isNumber
+}
+function calculate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case '+':
+            return firstNum + secondNum;
+        case '-':
+            return firstNum - secondNum;
+        case '*':
+            return firstNum * secondNum;
+        case '/':
+            return firstNum / secondNum;
+    }
+}
+function isOperador(operator) {
+    switch (operator) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
+}
+function isNumber(str) {
+    var maybeNum = parseInt(str);
+    var isNum = Boolean(!isNaN(maybeNum));
+    return isNum;
+}
+main();
--- index.ts
+++ index.ts
@@ -1,2 +1,88 @@
-console.log('I am writing Typescript: Eu estou escrevendo Typescripe');
+import { question } from "readline-sync";
+
+type Operator = '+'|'-'|'*'|'/';
+
+
+
+function main():void
+
+{
+    const primStr: string = question('Insira o primeiro numero:\n');
+    const operator :string=question('Insira o operador:\n');
+    const segString:string=question('Insira o segundo numero:\n');
+    //console.log('primStr');
+
+    const validInput : boolean = isNumber(primStr) && isOperador(operator) && isNumber(segString);
+    console.log(validInput);
+
+        if (validInput)
+        {    //console.log('isvalid.')
+            const firstNum: number = parseInt(primStr);
+            const secondNum:number = parseInt(segString);
+            const resultado =calculate(firstNum, operator as Operator, secondNum);
+            console.log(resultado)
+        }
+        else
+        {
+            console.log('\ninvalid input\n');
+            main();
+        }
+
+    //console.log (primString, operador, segString);
+    //const firstNum = isNumber(primStr);
+    //console.log(firstNum);
+    //const op = eOperador(operador)
+    //console.log(op);
     
+    //const validInput:boolean = isNumber
+
+}
+
+function calculate(firstNum: number, operator: Operator, secondNum: number)
+{
+        switch(operator)
+        {
+            case '+':
+                return firstNum+secondNum;
+                
+            case '-':
+                return firstNum - secondNum;
+
+            case '*':
+                return firstNum*secondNum;
+            case '/':
+                return firstNum/secondNum;
+
+                
+        }
+}
+
+function isOperador(operator:string):boolean
+{
+    switch(operator)
+    
+    {
+    
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        return true;
+    
+    default:
+         return false;
+    }
+    
+
+}
+
+
+function isNumber(str:string) :boolean
+{
+    const maybeNum = parseInt(str);
+    const isNum: boolean= Boolean(!isNaN(maybeNum));
+    return isNum;
+}
+
+
+main();
